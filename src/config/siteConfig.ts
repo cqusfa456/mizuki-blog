7d99f2c585632e2992016cc522195c1aafd1b709
CQU·SFA <CQUSFA@TUTAMAIL.COM>
2026-07-11 15:55:28 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -5,7 +5,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "CQU·SFA",
-	subtitle: "One demo website",
+	subtitle: "重庆大学多时空科幻协会",
 	siteURL: "https://cqusfa.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-07-11", // 站点开始运行日期，用于站点统计组件计算运行天数
 
